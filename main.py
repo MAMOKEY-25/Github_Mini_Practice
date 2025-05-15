e396b9986cd14b1555cef42b68ec60f80b9bc75d
junyeong-hub <bangjun301@gmail.com>
2025-05-16 01:22:36 +0900

    issue/#5team1_Main

---
+++ main.py
@@ -0,0 +1,29 @@
+def main():
+    print("계산기")
+    print("1: 덧셈")
+    print("2: 뺄셈")
+    print("3: 곱셈")
+    print("4: 나눗셈")
+
+
+    choice = input("연산을 선택하세요 (1/2/3/4): ")
+    a = float(input("첫 번째 숫자: "))
+    b = float(input("두 번째 숫자: "))
+
+    if choice == '1':
+        calc = 
+    elif choice == '2':
+        calc = 
+    elif choice == '3':
+        calc = 
+    elif choice == '4':
+        calc = 
+    else:
+        print("존재하지 않습니다.")
+        return
+
+    result = calc.calculate(a, b)
+    print(result)
+
+if __name__ == "__main__":
+    main()
