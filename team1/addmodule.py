322e99cc191f24e44940246d701b0ee042150232
rlaapxk <kimyuhan0318@gmail.com>
2025-05-16 02:25:06 +0900

    feat/#2 주석 추가

--- team1/addmodule.py
+++ team1/addmodule.py
@@ -1,10 +1,4 @@
+# 계산기 더하기 기능
 class Adder:
-    def add(self, a, b):
-        return a + b
-
-a = int(input("숫자 입력 a: "))
-b = int(input("숫자 입력 b: "))
-
-adder = Adder()
-result = adder.add(a, b)
-print("결과:", result)
+    def add(self, a, b): # 매개변수 a와 b를 받는다
+        return a + b # 결과값 리턴
