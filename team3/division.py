844bffc97bf0046a16dfd8d2d3ea19e71aed5376
이수종 <leesj9627@naver.com>
2025-05-16 21:18:40 +0900

    Comment: 나누기 주석 추가 이수종

--- team3/division.py
+++ team3/division.py
@@ -2,9 +2,9 @@
 
 class Division:
     def divide(self, a, b):
-        try:
+        try: # 정상적인 결과일 때 a/b 반환 
             return a / b
-        except ZeroDivisionError:
+        except ZeroDivisionError: # b가 0일때 
             return "0으로는 나눌 수 없습니다."
-        except TypeError:
+        except TypeError: # 매개변수 타입이 다를때 
             return "숫자만 입력하세요."
