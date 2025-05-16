20729f0a4f6f5a84c37e014b4d323c2576e90c91
YuSoYeong <1028ragon@naver.com>
2025-05-16 21:14:30 +0900

    Comment: 곱하기 주석 추가 유소영

--- team3/multiplication.py
+++ team3/multiplication.py
@@ -1,6 +1,10 @@
 class Multiplication:
+    # 두 수의 곱셈을 수행하는 메서드 정의
     def multiply(self, a, b):
         try:
+            # a와 b를 곱한 결과를 반환
             return a * b
         except TypeError:
+            # a나 b가 숫자가 아닌 경우 예외 처리
+            # 사용자에게 숫자만 입력해야 한다는 메세지를 반환
             return "숫자만 입력하세요."
