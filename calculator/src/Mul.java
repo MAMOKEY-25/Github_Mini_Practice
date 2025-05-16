2026914a1c4e9062c6d87a4d926955e896fdbf06
yeseul203 <kimyeseul153@gmail.com>
2025-05-16 16:16:26 +0900

    Feat/#16 곱셈코드 생성

--- calculator/src/Mul.java
+++ calculator/src/Mul.java
@@ -1,42 +1,18 @@
+//num1 * num2로 입력
 import java.util.Scanner;
-
-class mul {
-    //실수 곱하기
-    static double output(double num1, double num2) {
-        return num1 * num2;
-    }
-}
 
 public class Mul {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
 
-        //기호 확인
-        int index = input.indexOf('*');
-        if (index == -1) {
-            System.out.println("error");
-            return;
+        int num1 = scanner.nextInt();           //첫번째 숫자 입력
+        char index = scanner.next().charAt(0);  //기호 입력
+        int num2 = scanner.nextInt();           //두번째 숫자 입력
+
+        if (index == '*') {                     //입력한 기호가 *면 작동
+            System.out.println(num1*num2);
         }
 
-        String part1 = input.substring(0, index);               //* 앞 자르기
-        String part2 = input.substring(index + 1);    //* 뒤 자르기
-
-        //둘 중 하나라도 소수면 연산 결과를 소수로 처리
-        boolean isNum1 = part1.contains(".");   //입력된 숫자가 실수인지 확인하기
-        boolean isNum2 = part2.contains(".");
-
-        //둘 중 하나라도 소수면 소수로 출력 아니면 정수로 출력
-        if (isNum1 || isNum2) {
-            double num1 = Double.parseDouble(part1);
-            double num2 = Double.parseDouble(part2);
-            System.out.print(mul.output(num1, num2));
-        } else {
-            int num1 = Integer.parseInt(part1);
-            int num2 = Integer.parseInt(part2);
-            System.out.print(num1 * num2);
-        }
-
-        scanner.close();
+        scanner.close();                        //스캐너 닫기
     }
 }
