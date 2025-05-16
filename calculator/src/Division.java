49e7efed7baf3d711350be8615645b8f7707ca29
soni-ji <jisomglee4252@gmail.com>
2025-05-17 02:53:43 +0900

    [Remove] : 불필요한 파일 삭제

--- calculator/src/Division.java
+++ calculator/src/Division.java
@@ -7,3 +7,10 @@ public class Division {
 }
 
 // 값을 받아서 나누기 해서 리턴해줌.
+/*사용법 예시
+public class Main() {
+    public static void main(String[] args) {
+        double result = Division.divide(1, 5);         // 두 개의 매개변수를 받아 덧셈값을 result 변수에 저장
+        System.out.println("result = " / result);
+    }
+}*/
