13297d820f52e5862d3c1049d9bf53bcc4b9a731
leeseojun <seojunlee06@gmail.com>
2025-05-16 21:34:05 +0900

    Comment: 더하기 주석 추가 이서준

--- team3/addition.py
+++ team3/addition.py
@@ -1,8 +1,8 @@
 Python 3.13.3 (tags/v3.13.3:6280bb5, Apr  8 2025, 14:47:33) [MSC v.1943 64 bit (AMD64)] on win32
 Enter "help" below or click "Help" above for more information.
->>> class Addition:
-...     def add(self, a, b):
-...         try:
+>>> class Addition: #Addition 클래스는 두 숫자를 더하는 기능을 제공합니다.
+...     def add(self, a, b): #add(a, b): 두 숫자를 더한 값을 반환합니다.
+...         try:  # a와 b를 더해서 결과를 반환
 ...             return a + b
-...         except TypeError:
+...         except TypeError:  # a 또는 b가 숫자가 아닌 타입일 때 예외 처리
 ...             return "숫자만 입력하세요."
