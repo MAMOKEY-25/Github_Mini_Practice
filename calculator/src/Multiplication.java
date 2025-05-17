6c90803748f46192b831ad9f989c70d8198eafd3
yeseul203 <kimyeseul153@gmail.com>
2025-05-17 16:16:38 +0900

    [Feat] : 곱셈 모듈 기능 구현 완료

---
+++ calculator/src/Multiplication.java
@@ -0,0 +1,15 @@
+//곱셈 기능 담당 클래스
+public class Multiplication {
+    public static double multiply(int num1, int num2) {    //실수형 매개변수 2개 받는 매서드 static으로 선언
+        return num1 * num2;                                //  num1과 num2를 곱한 값 반환
+    }
+}
+
+/*사용 예시
+public class Main {
+    public static void main(String[] args) {
+        double result = Multiplication.multiply(4, 5);      // 두 수의 곱셈 결과 result변수에 저장
+        System.out.print("result = " + result);             // 결과 출력
+    }
+}
+*/
