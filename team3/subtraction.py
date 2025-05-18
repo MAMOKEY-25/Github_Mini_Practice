c7b5c2b4d67a01ab2f085e64286a3e8742647c6f
김도훈 <dohun0310@naver.com>
2025-05-18 19:16:25 +0900

    Revert "Feat :  issue /#7_Min 기능 추가 유태정 "

--- team3/subtraction.py
+++
@@ -1,6 +0,0 @@
-class Subtraction:
-    def subtract(self, a, b):
-        try:
-            return a - b # 숫자끼리 뺄셈 수행
-        except TypeError:
-            return "숫자만 입력하세요" # 숫자가 아닌 값이 입력되면 예외처리
