9f72b60ce97ef445e5a49bb9cab4c3a22aa4cafc
1028ragon <1028ragon@naver.com>
2025-05-19 20:35:39 +0900

    #8 Refactor: 문자열 반환 불가로 수정 유소영

--- team3/multiplication.py
+++ team3/multiplication.py
@@ -1,10 +1,6 @@
 class Multiplication:
-    # 두 수의 곱셈을 수행하는 메서드 정의
     def multiply(self, a, b):
-        try:
-            # a와 b를 곱한 결과를 반환
-            return a * b
-        except TypeError:
-            # a나 b가 숫자가 아닌 경우 예외 처리
-            # 사용자에게 숫자만 입력해야 한다는 메세지를 반환
+        if not (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+            # 문자열을 넣을 경우 숫자만 입력하라는 메세지 반환
             return "숫자만 입력하세요."
+        return a * b    # 정수형과 실수형인 a와 b를 곱해서 반환 
