c3ee97ad0950eecb45bf187714f2e96eafd95e0d
mubinchoe-mac <logan061175@gmail.com>
2025-05-19 19:26:48 +0900

    [Feat/#21] : Calculator Main 기능 추가

--- calculator/src/Main.java
+++ calculator/src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("프로그램 종료하려면 [ Q ] 을 입력하세요");
+        CalculatorModule calculator = new CalculatorModule(); // calculator 객체 생성
+        calculator.startCalculator();                         // 계산기 실행
+    }
 }
