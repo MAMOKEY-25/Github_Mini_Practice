2e7c7557d318bbe1106457b7d68a2f5327ca683d
junyeong-hub <bangjun301@gmail.com>
2025-05-20 21:42:51 +0900

    Fix/#37 merge오류 해결

---
+++ team1/div.py
@@ -0,0 +1,10 @@
+class Divide:
+    def div(self, num1, num2):
+        try: #예외처리
+            return num1 / num2
+        except ZeroDivisionError: #0이 들어오면 오류메세지를 출력하고 None 을 리턴함함
+            print("0으로 나눌 수 없습니다:다시 입력해 주세요")
+            return None
+        except TypeError: # 숫자가 아닌 다른 타입이 들어오면 오류 메세지를 출려갛고  None 을 리턴함
+            print("숫자를 입력해 주세요")
+            return None
